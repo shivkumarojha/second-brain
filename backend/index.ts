d56427755656dfdb97c655e3eeb12ddc8e947798
shivkumarojha <shivkmojha@gmail.com>
2025-03-18 12:59:54 +0530

    backend: basic express app

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello via Bun!");
+import express from "express"
+
+const app = express()
+
+app.get("/health", (req, res) => {
+    res.send("Hello")
+})
+
+app.listen((process.env.PORT), () => {
+    console.log('Server is listening at ' + process.env.PORT)
+})
