fb7cb354aeb8611250ae7159000163f23c7e8949
shivkumarojha <shivkmojha@gmail.com>
2025-03-20 22:26:19 +0530

    frontend: moved component to src and added basic button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
-import './App.css'
-
+import "./App.css"
+import Button from "./components/Button"
+import PlusIcon from "./icons/PlusIcon"
+import ShareIcon from "./icons/ShareIcon"
 function App() {
-
   return (
     <div>
-      <div className='text-2xl text-amber-500'>brainly</div>
+      <div className="text-2xl text-amber-500">brainly</div>
+      <div className="flex w-full">
+        <Button text="Add Content" startIcon={<PlusIcon />} variant="primary" />
+        <Button text="Share" variant="secondary" startIcon={<ShareIcon />} />
+      </div>
     </div>
   )
 }
---
+++ frontend/src/components/Button.tsx
@@ -0,0 +1,20 @@
+import { ReactElement } from "react"
+
+interface ButtonProps {
+  variant: "primary" | "secondary"
+  text: string
+  startIcon: ReactElement
+}
+
+const variantClasses = {
+  "primary": "bg-purple-500 text-white",
+  "secondary": "bg-purple-200 text-purple-600",
+}
+export default function Button({ variant, text, startIcon }: ButtonProps) {
+  return (
+    <button className={`${variantClasses[variant] + " " + "flex p-3 rounded-xl"}`}>
+      {startIcon}
+      {text}
+    </button>
+  )
+}
