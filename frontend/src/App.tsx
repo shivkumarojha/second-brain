82f2322c3d70172eb5ae66d01e659994cb8b1947
shivkumarojha <shivkmojha@gmail.com>
2025-03-28 23:33:34 +0530

    frontendi react router added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,8 @@
 import "./App.css"
-import Button from "./components/Button"
-import PlusIcon from "./icons/PlusIcon"
-import ShareIcon from "./icons/ShareIcon"
 function App() {
   return (
     <div>
-      <div className="text-2xl text-amber-500">brainly</div>
-      <div className="flex w-full">
-        <Button text="Add Content" startIcon={<PlusIcon />} variant="primary" />
-        <Button text="Share" variant="secondary" startIcon={<ShareIcon />} />
-      </div>
+      helll
     </div>
   )
 }
