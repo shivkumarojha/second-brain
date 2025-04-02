9ca9fa8a911817ad3fcde6594a6df90052389c75
shivkumarojha <shivkmojha@gmail.com>
2025-04-02 22:42:51 +0530

    sign up is working with janky version of using fetch api

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -1,10 +1,12 @@
 interface inputType {
   type: string
   placeholder: string
+  reference: React.RefObject<HTMLInputElement | null>
 }
-export default function InputBox({ type, placeholder }: inputType) {
+export default function InputBox({ type, placeholder, reference }: inputType) {
   return (
     <input
+      ref={reference}
       className="border-2 rounded-md p-2 my-2"
       type={type}
       placeholder={placeholder}
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,20 +1,39 @@
-import { useState } from "react"
+import { useRef } from "react"
 import AuthButton from "../components/AuthButtons"
 import InputBox from "../components/InputBox"
 
 export default function Signup() {
-  const [signupData, setSignupData] = useState({
-    username: "",
-    name: "",
-    password: "",
-  })
-  function handleSignUp() {}
+  const usernameRef = useRef<HTMLInputElement>(null)
+  const fullNameRef = useRef<HTMLInputElement>(null)
+  const passwordRef = useRef<HTMLInputElement>(null)
+
+  async function handleSignUp() {
+    const username = usernameRef.current?.value
+    const password = passwordRef.current?.value
+    const fullName = fullNameRef.current?.value
+    const user = await fetch("http://localhost:3000/api/v1/user/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+       name: fullName,
+      }),
+    })
+    console.log(user)
+  }
   return (
     <div className="h-screen flex flex-col justify-center items-center ">
       <div className="text-3xl font-semibold">Sign up</div>
-      <InputBox type="text" placeholder="username" />
-      <InputBox type="text" placeholder="Full Name" />
-      <InputBox type="password" placeholder="password" />
+      <InputBox reference={usernameRef} type="text" placeholder="username" />
+      <InputBox reference={fullNameRef} type="text" placeholder="Full Name" />
+      <InputBox
+        reference={passwordRef}
+        type="password"
+        placeholder="password"
+      />
       <AuthButton text="Sign Up" type="primary" onClick={handleSignUp} />
     </div>
   )
